7318c7149eaa00c3c4ae07b7806075ef77332548
Johan Berggren <jbn@nordu.net>
2011-06-28 11:06:21 +0200

    Added middleware and tempatetag

---
+++ themeit/middleware/themeit.py
@@ -0,0 +1,19 @@
+__author__ = 'jbn'
+import re
+from themes import themepatterns
+
+class Themeit(object):
+    def build_path(self, request):
+        path = (request.META['HTTP_HOST'], request.get_full_path())
+        return ''.join(path)
+
+    def process_request(self, request):
+        request.themeid = 1
+        path = self.build_path(request)
+        if re.search("site_media|favicon.ico", path):
+            return None
+        for pattern, themeid in themepatterns.items():
+            if re.search(pattern, path):
+                request.themeid = themeid
+                return None
+        return None
---
+++ themeit/templatetags/theming_tag.py
@@ -0,0 +1,4 @@
+__author__ = 'jbn'
+from django import template
+
+register = template.Library()
