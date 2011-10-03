e7448e9c4d4b98a0c21cd2caab418eb302462db4
Johan Berggren <jbn@nordu.net>
2011-10-03 10:10:36 +0200

    Added sites framework

--- themeit/middleware.py
+++ themeit/middleware.py
@@ -1,6 +1,6 @@
 __author__ = 'jbn'
 import re
-from themes import themepatterns
+from models import Theme
 
 class ThemeitMiddleware(object):
     def build_path(self, request):
@@ -8,13 +8,18 @@ class ThemeitMiddleware(object):
         return ''.join(path)
 
     def process_request(self, request):
+        try:
+            from themes import themepatterns
+        except ImportError:
+            return None
         request.themeid = 1
         path = self.build_path(request)
         if re.search("site_media|favicon.ico", path):
             return None
         for pattern, themeid in themepatterns.items():
             if re.search(pattern, path):
-                request.themeid = themeid
+                theme = Theme.objects.get(pk=themeid)
+                request.themeid = theme.id
+                request.site = theme.site
                 return None
         return None
-
--- themeit/templatetags/themeit_tag.py
+++ themeit/templatetags/themeit_tag.py
@@ -9,3 +9,8 @@ def get_theme(context):
     request = context['request']
     theme = Theme.objects.get(pk=request.themeid)
     return theme.css
+
+@register.simple_tag(takes_context=True)
+def get_site(context):
+    request = context['request']
+    return request.site
