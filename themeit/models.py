7875a552d7e76a97b179458bf049099f3255054e
Johan Berggren <jbn@nordu.net>
2011-10-19 13:36:35 +0200

    Added analytics TestField in models.py

--- themeit/models.py
+++ themeit/models.py
@@ -4,6 +4,7 @@ from django.contrib.sites.models import Site
 class Theme(models.Model):
     css = models.CharField(max_length=255, blank=True, null=True)
     site = models.ForeignKey(Site)
+    analytics = models.TextField(blank=True, null=True)
     def __unicode__(self):
         return '%s' % self.site
     class Admin:
